4bd4f481e9d9a43289d9533159aaadce6b7c9886
wcipriano <wagner@solucionarti.com.br>
2023-10-12 20:35:22 -0300

    Insert HTTP Methods Patch and DELETE

--- database.js
+++ database.js
@@ -24,13 +24,14 @@ class Database {
       ? `${req.params.resource}_${this.data_version[req.params.resource][idx]}`
       : "";
   };
-  get_resource_by_id = function (resource, id) {
+  get_resource_by_id = function (resource, id, ret_obj) {
+    ret_obj = ret_obj == undefined ? true : false;
     let res = null;
     if (resource in this.data) {
       for (let i = 0; i < this.data[resource].length; i++) {
         const item = this.data[resource][i];
         if (item.id == id) {
-          res = item;
+          res = ret_obj == true ? item : i;
           break;
         }
       }
@@ -39,11 +40,35 @@ class Database {
   };
 
   insert = function (resource, form) {
-    let id = this.data[resource].length + 1;
+    let id = this.data[resource][this.data[resource].length - 1].id + 1;
     form["id"] = id;
     this.data[resource].push(form);
     this.inc_data_version(resource);
     return form;
+  };
+
+  update = function (resource, form, id) {
+    let obj = this.get_resource_by_id(resource, id);
+    if (obj) {
+      for (let field in form) {
+        if (field in obj) {
+          obj[field] = form[field];
+        }
+      }
+      this.inc_data_version(resource);
+    }
+    return obj;
+  };
+
+  delete = function (resource, form, id) {
+    let obj_idx = this.get_resource_by_id(resource, id, false);
+    console.log("\n\nthis.data[resource]:: ", this.data[resource]);
+    if (obj_idx) {
+      this.data[resource].splice(obj_idx, 1);
+      this.inc_data_version(resource);
+    }
+    console.log("this.data[resource]:: ", this.data[resource]);
+    return obj_idx;
   };
 }
 
--- index.js
+++ index.js
@@ -58,7 +58,7 @@ server.use(function (req, res, next) {
 
 server.use(restify.plugins.conditionalRequest());
 
-// Routes
+// Routes GET
 server.get(`${path}/`, (req, res, next) => {
   res.send({ mensagem: "Welcome Restify AWS API TP1. Have fun!!!" });
 });
@@ -80,12 +80,34 @@ server.get(`${path}/:resource/:id`, (req, res, next) => {
   }
 });
 
-// POST - Insert Resource
+// Route POST - Insert Resource
 server.post(`${path}/:resource`, (req, res, next) => {
-  console.log("form:: ", req.body, typeof req.body);
   let item = db.insert(req.params.resource, req.body);
   res.send(201, item);
-  next();
+});
+
+// Route PATCH - Update Resource
+server.patch(`${path}/:resource/:id`, (req, res, next) => {
+  let item = db.update(req.params.resource, req.body, req.params.id);
+  if (item) {
+    res.send(200, item);
+  } else {
+    let err = new errs.NotFoundError(
+      `Resource /${req.params.resource}/${req.params.id} not found!`
+    );
+    res.send(err);
+  }
+});
+
+// Route DELETE - Remove Resource
+server.del(`${path}/:resource/:id`, (req, res, next) => {
+  let item = db.delete(req.params.resource, req.body, req.params.id);
+  if (item) {
+    res.send(200);
+  } else {
+    let err = new errs.BadRequestError("Item not found");
+    res.send(err);
+  }
 });
 
 // Server
